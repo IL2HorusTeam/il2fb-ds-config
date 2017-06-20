30da9d5f7430a62c36d43f40166877cbe6041248
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-06-20 21:25:53 +0300

    Define draft of Connection pane

--- src/App/Connection.jsx
+++ src/App/Connection.jsx
@@ -1,10 +1,128 @@
 import React, { Component } from 'react';
 
+import { Collapse, Form, Input, InputNumber, Row, Col, Button } from 'antd';
+
+const ButtonGroup = Button.Group;
+const FormItem = Form.Item;
+const Panel = Collapse.Panel;
+
+
+class Proxy extends Component {
+
+  render() {
+    const formItemLayout = {
+      labelCol: {
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        sm: { span: 20 },
+      },
+    };
+
+    return (
+      <Form layout="horizontal">
+        <FormItem
+          label="Host"
+          {...formItemLayout}
+        >
+          <Input placeholder="192.168.0.1" />
+        </FormItem>
+        <FormItem
+          label="Port"
+          {...formItemLayout}
+        >
+          <InputNumber
+            defaultValue={1080}
+            min={1}
+            max={65535}
+          />
+        </FormItem>
+        <FormItem
+          label="User"
+          {...formItemLayout}
+        >
+          <Input placeholder="username" />
+        </FormItem>
+        <FormItem
+          label="Password"
+          {...formItemLayout}
+        >
+          <Input placeholder="password" />
+        </FormItem>
+      </Form>
+    );
+  }
+
+}
+
 
 export default class Connection extends Component {
 
   render() {
-    return (<div>Connection</div>);
+    const formItemLayout = {
+      labelCol: {
+        sm: { span: 6 },
+      },
+      wrapperCol: {
+        sm: { span: 18 },
+      },
+    };
+
+    return (
+      <div className="tab-pane">
+        <h1>Player connection</h1>
+        <Form layout="horizontal">
+          <FormItem
+            label="Host"
+            {...formItemLayout}
+          >
+            <Input placeholder="192.168.0.1" />
+          </FormItem>
+          <FormItem
+            label="Port"
+            {...formItemLayout}
+          >
+            <InputNumber
+              defaultValue={21000}
+              min={1}
+              max={65535}
+            />
+          </FormItem>
+          <FormItem
+            label="Max clients"
+            {...formItemLayout}
+          >
+            <InputNumber
+              defaultValue={8}
+              min={1}
+              max={128}
+            />
+          </FormItem>
+          <FormItem
+            label="Throughput, B/sec"
+            {...formItemLayout}
+          >
+            <InputNumber
+              defaultValue={5000}
+              min={300}
+              max={1000000}
+            />
+            <ButtonGroup size="small">
+              <Button type="primary">Modem 28.8K</Button>
+              <Button type="primary">Modem 56K</Button>
+              <Button type="primary">DSL cable</Button>
+            </ButtonGroup>
+          </FormItem>
+        </Form>
+        <Collapse
+          bordered={false}
+        >
+          <Panel header="Proxy" key="1">
+            <Proxy />
+          </Panel>
+        </Collapse>
+      </div>
+    );
   }
 
 }
