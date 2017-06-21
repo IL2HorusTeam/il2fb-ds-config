dc75694ae0be556b3424af48f5f8bbfe974e4542
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-06-21 18:23:04 +0300

    Define draft of Import pane

--- src/App/Import.jsx
+++ src/App/Import.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
+import { Upload, Icon, message } from 'antd';
+
+const Dragger = Upload.Dragger;
+
+
+const props = {
+  name: 'file',
+  multiple: false,
+  showUploadList: false,
+  // action: '//jsonplaceholder.typicode.com/posts/',
+  // onChange(info) {
+  // },
+};
+
 
 export default class Import extends Component {
 
   render() {
-    return (<div>Import</div>);
+    return (
+      <div className="tab-pane">
+        <h1>Import configuration file</h1>
+        <div style={{ marginTop: 16, height: 180 }}>
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <Icon type="upload" />
+            </p>
+            <p className="ant-upload-text">Click or drag configuration file to this area to upload</p>
+            <p className="ant-upload-hint">Configuration file is located in the root of server's directory and named «confs.ini»</p>
+          </Dragger>
+        </div>
+      </div>
+    );
   }
 
 }
