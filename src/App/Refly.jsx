189fdba4c20e39cb9e5af2965adbc14764eb92c3
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-06-21 13:41:12 +0300

    Define draft of Refly pane

--- src/App/Refly.jsx
+++ src/App/Refly.jsx
@@ -1,10 +1,64 @@
 import React, { Component } from 'react';
 
+import { Switch, Form, InputNumber } from 'antd';
+
+const FormItem = Form.Item;
+
 
 export default class Refly extends Component {
 
   render() {
-    return (<div>Refly</div>);
+    const formItemLayout = {
+      labelCol: {
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        sm: { span: 10 },
+      },
+    };
+
+    return (
+      <div className="tab-pane">
+        <h1>Refly control</h1>
+        <Form layout="horizontal">
+          <FormItem
+            label="Allow refly"
+            {...formItemLayout}
+          >
+            <Switch checked />
+          </FormItem>
+          <FormItem
+            label="Death penalty, sec"
+            {...formItemLayout}
+          >
+            <InputNumber
+              defaultValue={0}
+              min={0}
+            />
+          </FormItem>
+          <FormItem
+            label="Death penalty multiplier"
+            {...formItemLayout}
+          >
+            <InputNumber
+              defaultValue={0.0}
+              min={0.0}
+              precision={1}
+              step={0.1}
+            />
+          </FormItem>
+          <FormItem
+            label="Death limit, #"
+            {...formItemLayout}
+          >
+            <InputNumber
+              defaultValue={-1}
+              min={-1}
+            />
+          </FormItem>
+        </Form>
+      </div>
+    );
   }
 
 }
