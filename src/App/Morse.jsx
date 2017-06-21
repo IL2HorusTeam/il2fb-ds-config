ea6155a32f308cd6ce6181394cb89cf171df1df0
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-06-21 13:29:49 +0300

    Define draft of Morse pane

--- src/App/Morse.jsx
+++ src/App/Morse.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
+import { Switch, Form } from 'antd';
+
+const FormItem = Form.Item;
+
 
 export default class Morse extends Component {
 
   render() {
-    return (<div>Morse</div>);
+    return (
+      <div className="tab-pane">
+        <h1>Morse messages</h1>
+        <Form layout="horizontal">
+          <FormItem>
+            <Switch checked /> Allow morse as text
+          </FormItem>
+          <FormItem>
+            <Switch /> Show morse as text
+          </FormItem>
+          <FormItem>
+            <Switch /> Block morse chat
+          </FormItem>
+        </Form>
+      </div>
+    );
   }
 
 }
