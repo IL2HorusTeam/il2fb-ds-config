87feea31157b7d551a194ab7fe8e00686e23c4ce
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-06-21 18:35:22 +0300

    Define draft of Export pane

--- src/App/Export.jsx
+++ src/App/Export.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
+import { Button } from 'antd';
+
 
 export default class Export extends Component {
 
   render() {
-    return (<div>Export</div>);
+    return (
+      <div className="tab-pane">
+        <h1>Export configuration file</h1>
+        <div style={{ textAlign: 'center', }}>
+        <Button
+          type="primary"
+          icon="download"
+          size="large"
+          style={{ width: '250px', height: '50px', }}
+        >Download «confs.ini»</Button>
+        </div>
+      </div>
+    );
   }
 
 }
