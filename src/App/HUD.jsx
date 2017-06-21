4ee316feadb6e27d76e31f974ac391e92a2fb8f6
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-06-21 13:26:13 +0300

    Define draft of HUD pane

--- src/App/HUD.jsx
+++ src/App/HUD.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
+import { Switch, Form } from 'antd';
+
+const FormItem = Form.Item;
+
 
 export default class HUD extends Component {
 
   render() {
-    return (<div>HUD</div>);
+    return (
+      <div className="tab-pane">
+        <h1>HUD</h1>
+        <Form layout="horizontal">
+          <FormItem>
+            <Switch /> Display mission-related messages
+          </FormItem>
+          <FormItem>
+            <Switch /> Display kill-related messages
+          </FormItem>
+          <FormItem>
+            <Switch /> Display HUD messages at bottom of screen
+          </FormItem>
+        </Form>
+      </div>
+    );
   }
 
 }
