929e580311f7fec9410ebc9fffcc18ccd75adc47
Alexander Oblovatniy <oblovatniy@gmail.com>
2018-10-05 22:32:55 -0700

    Revert "Refactor CreateStore"
    This reverts commit 8f9572cafb9ad96e09fa684b5f13a046c8bddbe2.

--- src/app/store/configureStore.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./configureStoreProd');
-} else {
-    module.exports = require('./configureStoreDev');
-}
--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 
 require('typeface-roboto')
-
-import configureStore from './App/store/configureStore'
 
 import './reset.css';
 import 'antd/dist/antd.css';
 
 import App from './App';
 import { getDefaults } from './App/actions'
+import rootReducer from './App/reducers'
 import './index.css';
 
+const loggerMiddleware = createLogger()
 
-const store = configureStore();
-
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
 store.dispatch(getDefaults())
 
 ReactDOM.render(
+  (
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
+  ),
   document.getElementById('root')
 );
